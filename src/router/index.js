07a73f9895ee64a8b547c63b0cafa91de01e82d9
Luke <68338769+wulukewu@users.noreply.github.com>
2025-02-22 22:48:11 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -29,7 +29,7 @@ const routes = [
     name: "DesktopWithPost",
     component: Desktop,
     meta : {
-      title:"Welcom to WEN's blog"
+      title:"Welcom to Luke's blog"
     }
   },
   {
@@ -37,7 +37,7 @@ const routes = [
     name: "Desktop",
     component: Desktop,
     meta : {
-      title:"Welcom to WEN's blog"
+      title:"Welcom to Luke's blog"
     }
   },
   {
